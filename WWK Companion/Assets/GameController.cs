8fba10f2d55e111110b642ec8357157b4860520d
Athenrome <athenrome@gmail.com>
2016-03-27 00:45:07 +1000

    Setup basic buttons

---
+++ WWK Companion/Assets/GameController.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+
+public class GameController : MonoBehaviour {
+
+    public List<string> SetupInstructions;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    public void RulesBtnPressed()
+    {
+
+    }
+
+    public void SetupBtnPressed()
+    {
+
+    }
+
+    public void ViewerBtnPressed()
+    {
+
+    }
+}
